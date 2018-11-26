43def9d3cde708444e4f4f96de1a17e6e1bba551
Willem A Voges <wavoges@gmail.com>
2018-11-26 15:19:10 +0200

    Update My_Function.py
    Listed all days of week, now only print specific day requested.

--- My_Function.py
+++ My_Function.py
@@ -2,29 +2,29 @@
 # Create your own function that prints all the days of the week
 # Create your own function that takes in a sentence and replaces every second word with the word “Hello”
 
-day = input("Pick a number between 1 and 7, q to quit: ")
+U_day = input("Pick a number between 1 and 7, q to quit: ")
 
 
-def day_name():
-    day = {"1": "Monday", "2": "Tuesday", "3": "Wednesday", "4": "Thursday", "5": "Friday", "6": "Saterday", "7": "Sunday", "q": "Quit"}
-    return day
+def day_name(U_day):
+    days = {"1": "Monday", "2": "Tuesday", "3": "Wednesday", "4": "Thursday", "5": "Friday", "6": "Saturday", "7": "Sunday", "q": "Quit"}
+    return days.get(U_day)
 
 
-if day == "1":
-    print(str(day_name.day("1")))
-elif day == "2":
-    print(str(day_name.day("2")))
-elif day == "3":
-    print(str(day_name.day("3")))
-elif day == "4":
-    print(str(day_name.day("4")))
-elif day == "5":
-    print(str(day_name.day("5")))
-elif day == "6":
-    print(str(day_name.day("6")))
-elif day == "7":
-    print(str(day_name.day("7")))
-elif day == "q":
+if U_day == "1":
+    print(str(day_name("1")))
+elif U_day == "2":
+    print(str(day_name("2")))
+elif U_day == "3":
+    print(str(day_name("3")))
+elif U_day == "4":
+    print(str(day_name("4")))
+elif U_day == "5":
+    print(str(day_name("5")))
+elif U_day == "6":
+    print(str(day_name("6")))
+elif U_day == "7":
+    print(str(day_name("7")))
+elif U_day == "q":
     exit()
 
 
@@ -34,7 +34,7 @@ lst = []
 for idx, word in enumerate(sentence.split()):
     if idx % 2 == 0:
         lst.append(word)
-        lst.append("hello")
+        lst.append("Hello")
 
 words = " ".join(lst)
 print(words)
