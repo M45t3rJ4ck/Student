abc33eb0d6eecdb779aa12fcbb865d960aba9121
Willem Adriaan Voges <wavoges@gmail.com>
2019-11-22 15:02:25 +0200

    Fixing Issuses
    Correcting Mistakes

--- Baby.py
+++ Baby.py
@@ -1,4 +1,5 @@
 print("!!Welcome To The Party!!")
+
 age = int(input("Please entr your age: "))
 if age < 18:
     print("You're not old enough")
--- Binary.py
+++ Binary.py
@@ -8,25 +8,17 @@
 # Remember to make use of the built-in functions found in the math module as well as lists.
 
 binary = []
-desimal = []
 
 bin2dec = int(input("Please enter your decimal number to convert: \n"))
-quotient = int(bin2dec / 2)
-print(str(quotient))
+
 remainder = int(bin2dec % 2)
-print(str(remainder))
 binary.append(remainder)
-print(str(binary))
-for num in range(quotient):
-    while quotient != 1:
-        quotient = int(quotient / 2)
-        print(str(quotient))
-        remainder = int(quotient % 2)
-        print(str(remainder))
+
+while bin2dec != 1:
+        bin2dec = int(bin2dec / 2)
+        remainder = int(bin2dec % 2)
         binary.append(remainder)
-        print(str(binary))
 
 binary = binary[::-1]
-binary.append(quotient)
-print(quotient)
-print(str(binary))
+
+print("Binary: " + str(binary))
